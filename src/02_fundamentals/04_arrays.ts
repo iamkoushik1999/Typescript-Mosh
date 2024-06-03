ab091f93ed83e36c041efe686c3cce3436748005
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-06-03 20:59:34 +0530

    04 arrays

---
+++ src/02_fundamentals/04_arrays.ts
@@ -0,0 +1,12 @@
+// let numbers: number[] = [1, 2, 3];
+
+// console.log(numbers);
+
+let numbers: number[] = [];
+
+// numbers[0] = 1;
+// numbers[1] = 2;
+
+numbers.forEach((n) => n);
+
+console.log(numbers);
