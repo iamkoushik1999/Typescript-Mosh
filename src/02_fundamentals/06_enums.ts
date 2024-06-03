9b1287405a61e3166a19d720df1f45b2b5468192
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-06-03 21:00:23 +0530

    06 enums

---
+++ src/02_fundamentals/06_enums.ts
@@ -0,0 +1,23 @@
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// PascalCase
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+// --------------------------------
+// const enum Size {
+//   Small = 1,
+//   Medium,
+//   Large,
+// }
+
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
