1a9e6f1f7e66ef26b79901719b147a84078aebcb
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-06-03 20:59:12 +0530

    03 data types

---
+++ src/02_fundamentals/03_dataTypes.ts
@@ -0,0 +1,8 @@
+let sales: number = 123_456_789;
+let course: string = "Typescript";
+let is_published: boolean = true;
+let level;
+// any type
+// function render(document: any) {
+//   console.log(document);
+// }
